16cee8483246b175d20cf7ecd739d16a34de6688
24clanTeam <31884711+24clan@users.noreply.github.com>
2020-06-09 04:40:20 +0100

    Tcodes.sh

---
+++ Tcodes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/24clanDevTeam/ADM-MANAGER-24clanDevTeam/master/install.sh; chmod 777 install.sh* && ./install.sh*
+
